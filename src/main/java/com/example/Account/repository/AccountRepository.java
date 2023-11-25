7f8767313754b8b3fab44c58a80c01dd94257494
jrnecki <129943670+jrnecki@users.noreply.github.com>
2023-11-25 09:13:41 +0900

    create account api test

--- src/main/java/com/example/Account/repository/AccountRepository.java
+++ src/main/java/com/example/Account/repository/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.example.Account.repository;
 
 import com.example.Account.domain.Account;
+import com.example.Account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 // spring에서 jpa를 쓰기쉽게 해주도록 만들어진 인터페이스
@@ -11,5 +13,8 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account,Long> {
     // 자동으로 쿼리 생성
     Optional<Account> findFirstByOrderByIdDesc(); // 맨 첫번째 값, id 내림차순으로
+    Integer countByAccountUser (AccountUser accountUser);
+
+    Optional<Account> findByAccountNumber(String AccountNumber);
 
 }
--- src/test/java/com/example/Account/controller/AccountControllerTest.java
+++ src/test/java/com/example/Account/controller/AccountControllerTest.java
@@ -1,18 +1,27 @@
 package com.example.Account.controller;
 
 import com.example.Account.domain.Account;
+import com.example.Account.dto.AccountDto;
+import com.example.Account.dto.CreateAccount;
+import com.example.Account.dto.DeleteAccount;
 import com.example.Account.type.AccountStatus;
 import com.example.Account.service.AccountService;
 import com.example.Account.service.RedisTestService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -21,11 +30,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AccountControllerTest {
     // Mock이지만 Bean으로 자동 등록되어서 위의 컨트롤러에 자동으로 주입된다.
     // @MockBean
+    @MockBean
     private AccountService accountService;
     @MockBean
     private RedisTestService redisTestService;
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void successCreateAccount() throws Exception {
+        // given
+        given(accountService.createAccount(anyLong(),anyLong()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registeredAt(LocalDateTime.now())
+                        .unRegisteredAt(LocalDateTime.now())
+                        .build());
+        // when
+        // then
+        mockMvc.perform(post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new CreateAccount.Request(1L,1000L)
+                )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print());
+    }
     @Test
     void successGetAccount() throws Exception {
         // given
@@ -41,5 +77,27 @@ class AccountControllerTest {
                 .andExpect(jsonPath("$.accountNumber").value("3456"))
                 .andExpect(jsonPath("$.accountStatus").value("IN_USE"))
                 .andExpect(status().isOk());
+    }
+    @Test
+    void successDeleteAccount() throws Exception {
+        // given
+        given(accountService.deleteAccount(anyLong(), anyString()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registeredAt(LocalDateTime.now())
+                        .build());
+
+        // when
+        // then
+        mockMvc.perform(delete("/account")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new DeleteAccount.Request(3333L,"1111111111")
+                        )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print());
     }
 }
