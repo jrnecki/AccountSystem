c5a14507af8a87a5a12a93de32a6cd08fa7c5ff3
jrnecki <129943670+jrnecki@users.noreply.github.com>
2023-12-12 09:43:24 +0900

    refactoring:

--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -71,7 +71,7 @@ public class AccountService {
         if(id < 0){
             throw new RuntimeException("Minus");
         }
-        return accountRepository.findById(id).get();
+        return accountRepository.findById(id).orElseThrow(() -> new AccountException(ACCOUNT_NOT_FOUND));
     }
 
     @Transactional
